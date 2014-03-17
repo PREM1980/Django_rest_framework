251bc8289b61c1d4a668efd6b964aaf9155fb717
prem <prem1pre@gmail.com>
2014-03-17 10:56:09 -0400

    Django bookmarks - Initial commit

---
+++ django_bookmarks/django_bookmarks/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from bookmarks.views import *
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    (r'^$',main_page),
+    # url(r'^$', 'django_bookmarks.views.home', name='home'),
+    #url(r'^django_bookmarks/', include('django_bookmarks.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
